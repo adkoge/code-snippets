63a207b3ee138422358aed3aabf45be98da1cc72
Alan Koger <akoger@westcarygroup.com>
2019-02-25 12:20:11 -0500

    Added snippet for enabling debugging to debug.log on a WordPress site

---
+++ WordPress/enable-debugging.php
@@ -0,0 +1,14 @@
+<?php
+/******************************
+ Place in WP-config for WordPress errors
+******************************/
+// Enable debugging
+define ('WP_DEBUG', true);
+
+// Write debug to /wp-content/debug.log file
+define ('WP_DEBUG_LOG', true);
+
+// Disable display of errors and warnings
+define('WP_DEBUG_DISPLAY', false);
+?>
+
